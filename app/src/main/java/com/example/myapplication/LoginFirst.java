fa26a865411a434763f5da1c6a2d3cf514b56d5f
anu jadav <anushamraojadav111@gmail.com>
2020-11-21 01:12:32 +0530

    La2-assignment

---
+++ app/src/main/java/com/example/myapplication/LoginFirst.java
@@ -0,0 +1,49 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public abstract class LoginFirst extends AppCompatActivity implements View.OnClickListener {
+    Button login;
+
+    EditText username,password;
+    DBHelper2 DB;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_first);
+        username=findViewById(R.id.username1);
+        password=findViewById(R.id.password1);
+        login=findViewById(R.id.login);
+        DB = new DBHelper2(this);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user.equals("")||pass.equals(""))
+                    Toast.makeText(LoginFirst.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkuserpass = DB.checkusernamepassword(user, pass);
+                    if(checkuserpass==true){
+                        Toast.makeText(LoginFirst.this, "Sign in successfull", Toast.LENGTH_SHORT).show();
+                        Intent intent  = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(LoginFirst.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+
+}
